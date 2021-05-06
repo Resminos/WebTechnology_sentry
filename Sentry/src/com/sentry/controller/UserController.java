1bdd73d527f94d7ff305a87d6f87c4350f4779e5
wrq77 <74533971+wrq77@users.noreply.github.com>
2021-05-06 18:03:27 +0200

    setting page

--- Sentry/src/com/sentry/controller/UserController.java
+++ Sentry/src/com/sentry/controller/UserController.java
@@ -1,5 +1,14 @@
 package com.sentry.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class UserController {
+	@RequestMapping("/setting")
+	public String showSettingPage() {
+		return "setting"; 
+	}
+
 
 }
